7be02a90c8db61cfbfa1c64a59f8a553d020d160
jemma johnston <jemjo@hotmail.co.uk>
2018-03-06 17:12:34 +0000

    my work today

---
+++ start_point/ruby_functions_practice.rb
@@ -0,0 +1,68 @@
+def return_10
+  return 10
+end
+
+
+
+def add(first_number, second_number)
+return(first_number + second_number)
+end
+
+
+
+def subtract(first_number, second_number)
+  return(first_number - second_number)
+end
+
+
+def multiply(first_number, second_number)
+  return(first_number * second_number)
+end
+
+
+
+def divide(first_number, second_number)
+return(first_number / second_number)
+end
+
+
+def length_of_string(actual_string)
+return(actual_string.length)
+end
+
+
+def join_string(string_1, string_2)
+  return(string_1 + string_2)
+end
+
+def string_as_number(string_1, string_2)
+  return(string_1.to_i + string_2.to_i)
+end
+
+
+def number_to_full_name(month_number)
+ return Date::MONTHNAMES[month_number]
+end
+
+
+def number_to_short_month_name(month_name)
+  return
+
+end
+
+
+def volume_of_cube()
+return
+end
+
+
+def volume_of_sphere(radius)
+  return 4/3*pi*(radius)^^3
+
+end
+
+
+def fahrenheit_to_celsius(number_1, number_2)
+  return(number_1 - 9 x 5 / 9 = number_2)
+
+end
