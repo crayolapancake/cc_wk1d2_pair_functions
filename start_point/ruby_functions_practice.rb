605eea1c4a0edd3ee5df3382bb64d314f4bf9951
jemma johnston <jemjo@hotmail.co.uk>
2018-03-06 18:33:39 +0000

    functions homework revision 2

--- start_point/ruby_functions_practice.rb
+++ start_point/ruby_functions_practice.rb
@@ -1,13 +1,11 @@
+
 def return_10
   return 10
 end
 
-
-
 def add(first_number, second_number)
-return(first_number + second_number)
+  return(first_number + second_number)
 end
-
 
 
 def subtract(first_number, second_number)
@@ -20,14 +18,13 @@ def multiply(first_number, second_number)
 end
 
 
-
 def divide(first_number, second_number)
-return(first_number / second_number)
+  return(first_number / second_number)
 end
 
 
 def length_of_string(actual_string)
-return(actual_string.length)
+  return(actual_string.length)
 end
 
 
@@ -35,34 +32,84 @@ def join_string(string_1, string_2)
   return(string_1 + string_2)
 end
 
-def string_as_number(string_1, string_2)
+def add_string_as_number(string_1, string_2)
   return(string_1.to_i + string_2.to_i)
 end
 
 
-def number_to_full_name(month_number)
- return Date::MONTHNAMES[month_number]
+def number_to_full_month_name(month)
+  case month
+  when 1
+      month_name = 'January'
+    when 2
+      month_name = 'February'
+    when 3
+    month_name = 'March'
+    when 4
+    month_name = 'April'
+    when 5
+    month_name = 'May'
+    when 6
+      month_name = 'June'
+    when 7
+    month_name = 'July'
+    when 8
+    month_name = 'August'
+    when 9
+    month_name ='September'
+    when 10
+      month_name = 'October'
+    when 11
+    month_name = 'November'
+    when 12
+    month_name = 'December'
+    end
+    return month_name
 end
 
 
-def number_to_short_month_name(month_name)
-  return
-
+def number_to_short_month_name(month)
+  case month
+  when 1
+    month_name = 'Jan'
+    when 2
+    month_name = 'Feb'
+    when 3
+    month_name = 'Mar'
+    when 4
+    month_name = 'Apr'
+    when 5
+    month_name = 'May'
+    when 6
+    month_name = 'Jun'
+    when 7
+    month_name = 'Jul'
+    when 8
+    month_name = 'Aug'
+    when 9
+    month_name = 'Sep'
+    when 10
+    month_name = 'Oct'
+    when 11
+    month_name = 'Nov'
+    when 12
+    month_name = 'Dec'
+end
+return month_name
 end
 
 
-def volume_of_cube()
-return
+def volume_of_cube(side)
+  return side**3
 end
 
 
 def volume_of_sphere(radius)
-  return 4/3*pi*(radius)^^3
-
+  return 4/3*pi*(radius)**3
 end
 
 
-def fahrenheit_to_celsius(number_1, number_2)
-  return(number_1 - 9 x 5 / 9 = number_2)
+def fahrenheit_to_celsius(fahrenheit)
+  return 5/9 * (fahrenheit - 32)
 
 end
